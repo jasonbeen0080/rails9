f850d143faaf293519cfbf28719df0bd9b02383f
jasonbeen0080 <iamnotzurabutkatsura@gmail.com>
2021-09-06 18:38:09 +0900

    rails9

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,14 +1,21 @@
 class List < ApplicationRecord
+attachment :image
+
+validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
+
+
+
 def change
   create_table :lists do |t|
     t.string :title
     t.string :body
-    
+
     t.timestamps
-  end 
-  en
+  end
 
-
+end
 
 
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '629d452a483e9002e9e479abb84eae1979836d4f0943120e6842e02ff0b05eceb28dd51065ba81c4c8131781d24105a86fc4dcb100ecc8b2aec587139344ca99'
